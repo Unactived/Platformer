5f904efc8b1d2e7ce888af7227f194767b8e7846
FrenchMasterSword <frenchmastersword@gmail.com>
2018-07-07 10:00:00 +0200

    window's dimensions, finished player physics prototype

--- conf.lua
+++ conf.lua
@@ -1,4 +1,8 @@
 function love.conf(t)
     t.title = "Platformer"
     t.version = "11.1"
+    t.window.width = 640
+    t.window.height = 480
+
+    t.console = true -- Windows
 end
--- main.lua
+++ main.lua
@@ -1,3 +1,11 @@
+bump = require 'libs.bump.bump'
+
+world = nil -- Storage
+
+ground_0 = {}
+ground_1 = {}
+
+
 -- Player object
 player = {
     x = 16,
@@ -14,22 +22,28 @@ player = {
     isJumping = false, -- action of jumping
     isGrounded = false, -- on the ground
     hasReachedMax = false,
-    jumpAcceleration = 500,
+    jumpAcceleration = 450,
     jumpMaxSpeed = 9.5,
     gravity = 80,
 
-    -- Needs to be loaded in love
+    -- Storage
     img = nil
 }
 
-
 function love.load()
+    world = bump.newWorld(16) -- tile size
+
     player.img = love.graphics.newImage('assets/sprites/player.png')
+
+    world:add(player, player.x, player.y, player.img:getWidth(), player.img:getHeight())
+
+    -- Test level
+    world:add(ground_0, 120, 360, 640, 16)
+    world:add(ground_1, 0, 448, 640, 32)
 end
 
 function love.update(dt)
-    player.x = player.x + player.xVelocity
-    player.y = player.y + player.yVelocity
+local prevX, prevY = player.x, player.y
 
     -- Friction
     player.xVelocity = player.xVelocity * (1 - math.min(dt * player.friction, 1))
@@ -53,6 +67,32 @@ function love.update(dt)
 
         player.isGrounded = false -- jumping
     end
+
+    local goalX = player.x + player.xVelocity
+    local goalY = player.y + player.yVelocity
+
+    player.filter = function(item, other)
+        local x, y, w, h = world:getRect(other)
+        local pX, pY, pW, pH = world:getRect(item)
+        local playerBottom = pY + pH
+        local otherBottom = y + h
+
+        if playerBottom <= y then
+            return 'slide'
+        end
+    end
+
+    player.x, player.y, collisions, len = world:move(player, goalX, goalY, player.filter)
+
+    for i, coll in ipairs(collisions) do
+        if coll.touch.y > goalY then
+            player.hasReachedMax = true
+            player.isGrounded = false
+        elseif coll.normal.y < 0 then
+            player.hasReachedMax = false
+            player.isGrounded = true
+        end
+    end
 end
 
 function love.keypressed(key)
@@ -63,4 +103,6 @@ end
 
 function love.draw(dt)
     love.graphics.draw(player.img, player.x, player.y)
+    love.graphics.rectangle('fill', world:getRect(ground_0))
+    love.graphics.rectangle('fill', world:getRect(ground_1))
 end
