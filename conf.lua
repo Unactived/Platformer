e2965dc039a7ef64d1acd07aa90261bfc2e3a229
FrenchMasterSword <frenchmastersword@gmail.com>
2018-08-06 14:32:06 +0200

    Config optimisation and icon

--- conf.lua
+++ conf.lua
@@ -1,8 +1,15 @@
 function love.conf(t)
     t.title = "Platformer"
+    t.window.icon = "/assets/icon.png"
     t.version = "11.1"
     t.window.width = 600
     t.window.height = 432
+    t.window.fullscreentype = "exclusive"
+    
+    t.accelerometerjoystick = false
+    t.modules.joystick = false
+    t.modules.physics = false
+    t.modules.touch = false
 
     t.console = true -- Windows
 end
